25f6714f1850592ce72800e960dcc15a0e2e6923
mittymatty <mittymattychickennugget@gmail.com>
2026-08-27 21:21:10 +1200

    Added script for terminal and updated visuals

---
+++ Scenes/Puzzles/base_puzzle_terminal.gd
@@ -0,0 +1,19 @@
+class_name Terminal extends Node3D
+
+@export var label: Label3D
+#@export var game
+
+func _ready() -> void:
+	label.hide()
+
+func interact () -> void:
+	play_game()
+
+func show_prompt () -> void:
+	label.show()
+
+func hide_prompt () -> void:
+	label.hide()
+
+func play_game () -> void:
+	print("Opened Game")
