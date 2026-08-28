d903a0412f41a321c78210a27c4a20ec8ab8fbcb
mittymatty <mittymattychickennugget@gmail.com>
2026-08-28 21:07:06 +1200

    Wiring Mini-game made

--- Scenes/Puzzles/socket.gd
+++ Scenes/Puzzles/socket.gd
@@ -1,15 +1,12 @@
-extends Button
+class_name Socket extends Button
 
 @export var socket_colour: String
 @export var socket_side: String
 
 signal socket_pressed(socket)
 
-
-
 func _ready():
 	pressed.connect(_on_pressed)
-
 
 func _on_pressed():
 	socket_pressed.emit(self)
