fb249390f135725f0a60588a069e989607b88be8
mittymatty <mittymattychickennugget@gmail.com>
2026-08-30 09:06:16 +1200

    Casette Player: Part 2
    Added the ability to carry casettes and delay their audio being played until inserted in a terminal. Subtitles not done.

--- Scenes/Puzzles/puzzle_terminal.gd
+++ Scenes/Puzzles/puzzle_terminal.gd
@@ -4,17 +4,37 @@ class_name Terminal extends Node3D
 @export var puzzle_scene: PackedScene
 @export var door_ID_to_open : int
 
+@onready var original_text: String = label.text
+@onready var voice_memo_player: AudioStreamPlayer3D = $VoiceMemoPlayer
+@onready var insert_casette_noise: AudioStreamPlayer3D = $InsertCasetteNoise
+
+func get_has_casette () -> bool:
+	return true if Status.stored_casette_stream else false
+
 func _ready() -> void:
 	label.hide()
 
 func interact () -> void:
-	play_game()
+	if !get_has_casette():
+		play_game()
+	else:
+		play_stored_casette()
 
 func show_prompt () -> void:
+	label.text = original_text if get_has_casette() else "Press E to Insert Casette"
+	
 	label.show()
 
 func hide_prompt () -> void:
 	label.hide()
+
+func play_stored_casette() -> void:
+	voice_memo_player.stream = Status.stored_casette_stream
+	Status.set_stored_casette_stream(null)
+	SignalBus.emit_insert_casette()
+	voice_memo_player.play()
+	insert_casette_noise.play()
+	#Handle subtitles
 
 func play_game () -> void:
 	if !puzzle_scene: return
--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody3D
 
 @onready var player_camera: Camera3D = $Head/PlayerCamera
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var casette: Node3D = $Casette
 
 @export var is_audible : bool = true
 
@@ -26,6 +27,17 @@ const MIN_AUDIBLE_SPEED : float = 3.0
 # Changing variables
 var bob_progress : float = 0.0
 var current_speed : float = 0.0
+
+#region casette
+
+func show_held_casette(audio_stream: AudioStream) -> void:
+	Status.set_stored_casette_stream(audio_stream)
+	casette.show()
+
+func hide_held_casette() -> void:
+	casette.hide()
+
+#endregion
 
 #region handlers
 
@@ -52,6 +64,10 @@ func decide_is_audible () -> bool:
 	return current_speed >= MIN_AUDIBLE_SPEED
 
 #endregion
+
+func _ready() -> void:
+	SignalBus.pickup_casette.connect(show_held_casette)
+	SignalBus.insert_casette.connect(hide_held_casette)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
