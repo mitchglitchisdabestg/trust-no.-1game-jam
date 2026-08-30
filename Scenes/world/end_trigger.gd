469d5f8b88a510ea0cee93065dfd43d27917b090
mitchglitchisdabestg <supg293@gmail.com>
2026-08-30 21:40:45 +1200

    Fixed the end cutscne

--- Scenes/world/end_trigger.gd
+++ Scenes/world/end_trigger.gd
@@ -11,6 +11,13 @@ func _on_body_entered(body):
 	print("TRIGGER ENTERED: ", body.name)
 
 	if body.name == "Player" and not triggered:
+		
 		triggered = true
 		print("PLAYING CUTSCENE!")
 		animation_player.play("end_cutscene")
+		body.set_physics_process(false)
+		await animation_player.animation_finished
+		
+		await get_tree().create_timer(0.5).timeout
+		
+		get_tree().change_scene_to_file("res://Scenes/main_menu/main_menu.tscn")
